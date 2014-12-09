22c393a5b9f7e16bfae4622c59e420f6e53ef919
minghao <horizon3385@gmail.com>
2014-12-08 20:42:09 -0500

    initiate repo

---
+++ README.md
@@ -0,0 +1,70 @@
+## TSP-Simulated Anneal (SA)
+
+### Introduction
+Simulated Anneal (SA) algorithm to solve a NP-hard problem ---- Travelling Salesman's Problem (TSP). 
+
+The algorithm follows the assumptions:
+
+* There always exists a path between two arbitary cities
+* Any path is available in both directions
+* Eculidean distance from cities' X-Y coordinates
+* ……
+
+Script `SimulatedAnneal.py` is built with `Python` standard library.
+
+### Usage
+#### Input
+Input city coordinates `cities-coordinate.json`  file in the following JSON format:
+```
+{
+	"x": [52.66, 73.66,...],
+	"y": [10.19, 20.85,...]
+}
+```
+#### Basic Usage
+In command line run:
+
+```
+cat cities-coordinate.json | python SimulatedAnneal.py
+```
+#### Arguments
+Some arguments provided:
+
+* `--maxTemperature`: initiate starting temperature, default `30.0`;
+* `--pho`: decrease rate of temperature, default `.99`;
+* `--length`: initiate starting length for maximum temperature, default `100`;
+* `--length_increment`: length increment after each time temperature decrease, default `100`, 
+* `--minTemperature`: minimum temperature, default `0.001`
+* `--maxIterations`: maximum iteration steps for annealing procedure, default `10**8`
+
+### Results
+
+My best result:
+
+```
+[
+  94, 99, 91, 45, 0, 87, 79, 63, 46, 82, 
+  55, 40, 37, 23, 59, 3, 10, 49, 24, 17, 
+  36, 71, 95, 52, 22, 76, 70, 90, 88, 6, 
+  28, 73, 61, 39, 43, 32, 20, 16, 89, 97, 
+  54, 75, 38, 65, 41, 11, 81, 19, 80, 72, 
+  68, 26, 64, 60, 13, 34, 31, 42, 18, 14, 
+  83, 33, 27, 93, 86, 8, 62, 84, 44, 29, 
+  77, 5, 98, 2, 12, 57, 15, 21, 7, 25, 
+  69, 48, 35, 96, 78, 50, 30, 4, 47, 51, 
+  58, 9, 85, 67, 92, 53, 66, 1, 74, 56, 94
+]
+```
+
+![alt text](https://github.com/horizon3385/SimulatedAnneal_TSP/best_record.png)
+
+### Limitation
+The final optimized routine varies every time. Theoretically, the path converge to the shortest path *in probabiliy*. See more details in [wiki](//http://en.wikipedia.org/wiki/Simulated_annealing).
+
+An optimized path from grid points:
+
+run `python cities_grid.py | python SimulatedAnneal.py --maxTemperature 3 --maxIterations 5000000`
+
+![alt text](https://github.com/horizon3385/SimulatedAnneal_TSP/Grid.png)
+
+**Good Luck!**
---
+++ SimulatedAnneal.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import division
+from random import sample
+
+
+class NewPath(object):
+    """NewPath class includes:
+
+    1. Generate a neighbour path from the old path;
+    2. Return the difference between new and old path;
+    3. A static method to return the total length of one path.
+    """
+    
+    def __init__(self, old, cnt):
+        new = old[:]
+        self.old = old
+        self.loc0, self.loc1 = sample(range(1, cnt), 2)
+        new[self.loc0], new[self.loc1] = new[self.loc1], new[self.loc0]
+        self.new = new
+
+    def diff(self, matrix):
+        """return the difference between new and old path:
+        diff = old_length - new _length
+        """
+        old_sum = 0
+        new_sum = 0
+        for index in [self.loc0, self.loc1]:
+            for sub_index in [-1, 1]:
+                old_sum += matrix[self.old[index + sub_index]][self.old[index]]
+                new_sum += matrix[self.new[index + sub_index]][self.new[index]]
+        return old_sum - new_sum
+
+    @staticmethod
+    def length_total(path, matrix):
+        total = 0
+        for i in range(len(path) - 1):
+            total += matrix[path[i]][path[i + 1]]
+        return total
+
+    @staticmethod
+    def revert_part(path, index1, index2):
+        to_revert = path[index1: index2]
+        to_revert.reverse()
+        new = path[:index1]
+        new = new + to_revert
+        new = new + path[index2:]
+        return new
+
+
+def main():
+
+    import sys
+    import json
+    import argparse
+    from random import random
+    from math import sqrt, exp
+
+    #---- arguments ----#
+    parser = argparse.ArgumentParser(
+        description="""
+        Travelling Salesman's Problem (TSP)
+        with simulated annealing (SA) algorithm
+
+        Input cities' coordinate from JSON file.
+        Input JSON file should be like:
+        {
+            "x": [52.66, ...],
+            "y": [10.19, ...]
+        }""", formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--maxIterations', type=int, default=10**8, help='Maximum iteration steps')
+    parser.add_argument('--maxTemperature', type=float, default=30.0, help='Initiate temperature')
+    parser.add_argument('--minTemperature', type=float, default=0.001, help='Minimum temperature')
+    parser.add_argument('--pho', type=float, default=0.99, help='schedule temperature structure')
+    parser.add_argument('--length', type=int, default=100, help='initiate length in each specified temperature')
+    parser.add_argument('--length_increment', type=int, default=100, help='increment in step length in each specified temperature')
+    args = parser.parse_args()    
+
+    #---- import cities coordinate ----#
+    cities = json.load(sys.stdin)
+    x = cities.get('x')
+    y = cities.get('y')
+    cnt = len(x)
+    if not cnt == len(y):
+        print >> sys.stderr, "check cities coordinate"
+        sys.exit(2)
+
+    #---- distance matrix ----#
+    dis_matrix = []
+    for i in range(cnt):
+        row = []
+        for k in range(cnt):
+            row.append(sqrt((x[i] - x[k])**2 + (y[i] - y[k])**2))
+        dis_matrix.append(row)  
+         
+    #---- initiate algorithm ----#
+    path = sample(range(cnt), cnt)
+    path.append(path[0])
+    iteration = 0
+    temperature = args.maxTemperature
+    schedule_length = 0
+    while iteration < args.maxIterations and temperature > args.minTemperature:
+        try:
+            schedule_length += 1
+            iteration += 1            
+            new_path = NewPath(path, cnt)
+            difference = new_path.diff(matrix=dis_matrix)
+            if difference > 0 or random() < exp(difference / temperature):
+                path = new_path.new
+            if schedule_length % args.length == 0:
+                temperature *= args.pho
+                args.length += args.length_increment
+                schedule_length = 0
+                print 'current iteration:\t%.2E\t' \
+                    'current temperature:\t%.2f\t' \
+                    'current total length:\t%.4f' % (iteration, temperature, new_path.length_total(path, matrix=dis_matrix))
+        except KeyboardInterrupt:
+            break
+
+    print '\noptimized path:'
+    print path
+    print 'total length: %.4f' % new_path.length_total(path, matrix=dis_matrix)
+    
+    #path = [94, 99, 91, 45, 0, 87, 79, 63, 46, 82, 55, 40, 37, 23, 59, 3, 10, 49, 24, 17, 36, 71, 95, 52, 22, 76, 70, 90, 88, 6, 28, 73, 61, 39, 43, 32, 20, 16, 89, 97, 54, 75, 38, 65, 9, 58, 51, 47, 4, 30, 50, 78, 96, 35, 48, 69, 25, 7, 21, 15, 57, 12, 2, 98, 5, 77, 29, 44, 84, 62, 8, 86, 93, 27, 33, 83, 14, 18, 42, 31, 34, 13, 60, 64, 26, 68, 72, 80, 19, 81, 11, 41, 85, 67, 92, 53, 66, 1, 74, 56, 94]
+    #new_path = NewPath(path[:100], 100)
+    #---- solve cross path ----#
+    def one_step_more(path):
+        """check all potential path to solve cross path"""
+        for i in range(1, len(path) - 4):
+            for k in range(i + 3, len(path) - 1):
+                new = new_path.revert_part(path, i, k)
+                if new_path.length_total(new, matrix=dis_matrix) < new_path.length_total(path, matrix=dis_matrix):
+                    path = new
+        return path
+
+    #---- recursive function??? ----#
+    while True:
+        if path == one_step_more(path):
+            break
+        path = one_step_more(path)
+    
+    print '\noptimized cross path:'
+    print path
+    print 'updated total length: %.4f' % NewPath.length_total(path, matrix=dis_matrix)
+
+
+if __name__ == '__main__':
+    main()
